46c572d56fd2ba85ce6c8ac564f7c5858f75b3fa
mrLuisFer <mrluisfeer@gmail.com>
2021-09-22 15:02:03 -0500

    [feat] add details

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 import QuoteContent from "./components/QuoteContent.vue";
+import Header from "./components/Header.vue";
 </script>
 
 <template>
+  <Header />
   <QuoteContent />
 </template>
 
 <style>
+body {
+  background: rgb(247, 247, 247);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  width: 100%;
+  min-height: 100vh;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<script setup></script>
+
+<template>
+  <header class="header">
+    <a href="/all" class="flex quotes-all">
+      <span class="material-icons"> bolt </span>
+      All quotes
+    </a>
+    <div class="flex quotes-random">
+      <p>Random</p>
+      <span class="material-icons"> shuffle </span>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.flex {
+  display: flex;
+  align-items: center;
+}
+
+.quotes-all {
+  cursor: pointer;
+}
+.quotes-random {
+  cursor: pointer;
+}
+</style>
