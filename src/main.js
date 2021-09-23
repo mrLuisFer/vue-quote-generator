5f87c49f6ec78aa5818a6315ea332d47ea8ae13a
mrLuisFer <mrluisfeer@gmail.com>
2021-09-23 14:24:03 -0500

    [feat] add vue router and fix details

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import Home from './views/Home.vue';
 
-createApp(App).mount('#app')
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+  base: '/',
+});
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<script>
+import QuoteContent from '../components/QuoteContent.vue'
+import Header from '../components/Header.vue'
+
+export default {
+  name: 'home',
+}
+</script>
+
+<template>
+  <div class="home">
+    <Header />
+    <QuoteContent />
+  </div>
+</template>
+
+<style scoped></style>
