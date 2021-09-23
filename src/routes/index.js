7e7170a40e09eee19adab4779f281091866fec02
mrLuisFer <mrluisfeer@gmail.com>
2021-09-23 17:27:01 -0500

    [feat] add allQuotes route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
 import Home from '../views/Home.vue';
+import AllQuotes from '../views/AllQuotes.vue';
 
 export const routes = [
   {
     path: '/',
     name: 'home',
     component: Home,
+  },
+  {
+    path: '/quotes',
+    name: 'quotes',
+    component: AllQuotes,
   },
 ];
---
+++ src/views/AllQuotes.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="container">
+    <p>All</p>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
