6e40f2284c594769eef946ba2845d80bb2f2097b
mrLuisFer <mrluisfeer@gmail.com>
2021-09-23 16:37:59 -0500

    [chore] fixing dependencies

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import { createApp } from 'vue';
-import VueRouter from 'vue-router';
+import VueRouter, { createWebHistory } from 'vue-router';
 import App from './App.vue';
 import Home from './views/Home.vue';
 import { routes } from './routes';
 
 const router = new VueRouter({
+  history: createWebHistory(),
   mode: 'history',
   routes,
   base: '/',
 });
+
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
