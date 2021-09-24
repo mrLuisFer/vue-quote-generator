70ac8bba706cd2d9ed80fcd5f97075b0390dee46
mrLuisFer <mrluisfeer@gmail.com>
2021-09-24 14:11:01 -0500

    [feat] adding methods and quotegarden pkg

--- src/components/QuoteContent.vue
+++ src/components/QuoteContent.vue
@@ -1,4 +1,25 @@
-<script setup></script>
+<script>
+import { randomQuote } from 'quotegarden'
+
+export default {
+  name: 'QuoteContent',
+  data() {
+    return {
+      quotes: [],
+    }
+  },
+  mounted() {
+    this.getRandomQuote()
+  },
+  methods: {
+    getRandomQuote() {
+      randomQuote().then((quote) => {
+        this.quotes.push(quote)
+      })
+    },
+  },
+}
+</script>
 
 <template>
   <p>Quote</p>
