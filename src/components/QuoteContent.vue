340608852ed208d5f10ef137ce1227f74932499d
mrLuisFer <mrluisfeer@gmail.com>
2021-09-24 21:41:23 -0500

    [feat] add styles in quoteContent

--- src/components/QuoteContent.vue
+++ src/components/QuoteContent.vue
@@ -26,17 +26,48 @@ export default {
   <article
     v-for="quote in quotes"
     :key="quote._id"
-    class="quote-container"
+    class="quote"
     id="#quote-content"
   >
-    <h1 class="quote-text">
-      <blockquote>
-        {{ quote.content }}
-      </blockquote>
-    </h1>
-    <h2 class="quote-author">{{ quote.author }}</h2>
-    <p class="quote-author-slug">{{ quote.authorSlug }}</p>
+    <blockquote class="quote-text">"{{ quote.content }}"</blockquote>
+    <div class="quote-author">
+      <h1 class="quote-author-name">{{ quote.author }}</h1>
+      <p class="quote-author-slug">{{ quote.authorSlug }}</p>
+    </div>
   </article>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.quote {
+  margin: 0 auto;
+  display: block;
+  max-width: 600px;
+
+  &-text {
+    height: 200px;
+    font-size: 1.7rem;
+    line-height: 1.5;
+    padding: 1rem 4rem;
+    border-left: 5px solid var(--primary);
+    display: flex;
+    align-items: center;
+  }
+
+  &-author {
+    margin: 4rem 0 0 4rem;
+
+    &-name {
+      font-size: 1rem;
+      font-weight: 600;
+      margin-bottom: 0.5rem;
+      text-transform: capitalize;
+    }
+
+    &-slug {
+      font-weight: 400;
+      font-size: 0.8rem;
+      text-transform: lowercase;
+    }
+  }
+}
+</style>
