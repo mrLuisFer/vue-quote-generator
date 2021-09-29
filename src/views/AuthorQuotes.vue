51be819014ebec8a104e011e1bfb13dbee134b03
mrLuisFer <mrluisfeer@gmail.com>
2021-09-28 21:17:31 -0500

    [feat] add list of single quote author

--- src/views/AuthorQuotes.vue
+++ src/views/AuthorQuotes.vue
@@ -3,7 +3,7 @@ export default {
   data() {
     return {
       authorParams: this.$route.params.author,
-      auhorQuotes: []
+      authorQuoteData: [],
     }
   },
   mounted() {
@@ -14,17 +14,28 @@ export default {
       const url = `https://quotable.io/quotes?author=${this.authorParams}`
       const res = await fetch(url)
       const data = await res.json()
-      this.authorQuotes = data
-      // console.log(data)
+      this.authorQuoteData = data
+      console.log(data)
     },
   },
 }
 </script>
 
 <template>
-  <section class="author-quotes">
-    <p>author: {{ authorParams }}</p>
-    
+  <section class="author-container">
+    <h1 class="author-title">{{ authorParams }}</h1>
+    <p class="author-count">{{ authorQuoteData.count }} Most famous quotes:</p>
+    <article>
+      <ul class="author-list">
+        <li
+          class="author-list-item"
+          v-for="authorQuote in authorQuoteData.results"
+          :key="authorQuote.author"
+        >
+          {{ authorQuote.content }}
+        </li>
+      </ul>
+    </article>
   </section>
 </template>
 
