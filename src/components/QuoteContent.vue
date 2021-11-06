930a9a32651cd45d5abdf23fc5e2df83d63827c5
mrLuisFer <mrluisfeer@gmail.com>
2021-11-05 19:07:41 -0600

    separate loader into a single component

--- src/components/QuoteContent.vue
+++ src/components/QuoteContent.vue
@@ -1,15 +1,18 @@
 <script>
 import QuoteAuthor from './QuoteAuthor.vue'
+import Loader from './common/Loader.vue'
 
 export default {
   el: '#quote-content',
   name: 'QuoteContent',
   components: {
     'quote-author': QuoteAuthor,
+    'common-loader': Loader,
   },
   data() {
     return {
       quotes: [],
+      loading: true,
     }
   },
   mounted() {
@@ -21,32 +24,46 @@ export default {
       const res = await fetch(url)
       const quote = await res.json()
       this.quotes.push(quote)
+      this.loading = false
     },
   },
 }
 </script>
 
 <template>
-  <div
-    v-for="quote in quotes"
-    id="#quote-content"
-    :key="quote._id"
-    class="quote"
-  >
-    <blockquote class="quote-text">"{{ quote.content }}"</blockquote>
-    <quote-author :authorName="quote.author" :authorSlug="quote.authorSlug" />
+  <div v-if="!loading">
+    <div
+      v-for="quote in quotes"
+      id="#quote-content"
+      :key="quote._id"
+      class="quote"
+    >
+      <blockquote class="quote-text">"{{ quote.content }}"</blockquote>
+      <quote-author :authorName="quote.author" :authorSlug="quote.authorSlug" />
+    </div>
   </div>
+  <div v-else>
+		<common-loader></common-loader>
+	</div>
 </template>
 
 <style lang="scss">
 $sm: 640px;
+$md: 768px;
 
 .quote {
   margin: 0 auto;
   display: block;
+  margin-top: 2rem;
 
   @media (min-width: $sm) {
     max-width: 600px;
+    margin-top: 1rem;
+  }
+
+  @media (min-width: $md) {
+    max-width: 700px;
+    margin-top: 2.5rem;
   }
 
   &-text {
--- src/views/MoreQuotes.vue
+++ src/views/MoreQuotes.vue
@@ -1,6 +1,7 @@
 <script>
 import MoreQuotesFooter from '../components/MoreQuotesView/MoreQuotesFooter.vue'
 import MoreQuotesList from '../components/MoreQuotesView/MoreQuotesList.vue'
+import Loader from '../components/common/Loader.vue'
 
 export default {
   el: '#more-quotes',
@@ -8,6 +9,7 @@ export default {
   components: {
     'more-quotes-footer': MoreQuotesFooter,
     'more-quotes-list': MoreQuotesList,
+    'common-loader': Loader,
   },
   data() {
     return {
@@ -60,81 +62,9 @@ export default {
       ></more-quotes-footer>
     </div>
     <div v-else>
-      <div class="loader">Loading...</div>
+      <common-loader></common-loader>
     </div>
   </section>
 </template>
 
-<style scoped>
-.loader {
-  font-size: 10px;
-  margin: 50px auto;
-  text-indent: -9999em;
-  width: 11em;
-  height: 11em;
-  border-radius: 50%;
-  background: #ff943d;
-  background: -moz-linear-gradient(
-    left,
-    #ff943d 10%,
-    rgba(255, 148, 61, 0) 42%
-  );
-  background: -webkit-linear-gradient(
-    left,
-    #ff943d 10%,
-    rgba(255, 148, 61, 0) 42%
-  );
-  background: -o-linear-gradient(left, #ff943d 10%, rgba(255, 148, 61, 0) 42%);
-  background: -ms-linear-gradient(left, #ff943d 10%, rgba(255, 148, 61, 0) 42%);
-  background: linear-gradient(to right, #ff943d 10%, rgba(255, 148, 61, 0) 42%);
-  position: relative;
-  -webkit-animation: load3 1.4s infinite linear;
-  animation: load3 1.4s infinite linear;
-  -webkit-transform: translateZ(0);
-  -ms-transform: translateZ(0);
-  transform: translateZ(0);
-}
-.loader:before {
-  width: 50%;
-  height: 50%;
-  background: #ff943d;
-  border-radius: 100% 0 0 0;
-  position: absolute;
-  top: 0;
-  left: 0;
-  content: '';
-}
-.loader:after {
-  background: #ff943d;
-  width: 75%;
-  height: 75%;
-  border-radius: 50%;
-  content: '';
-  margin: auto;
-  position: absolute;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-}
-@-webkit-keyframes load3 {
-  0% {
-    -webkit-transform: rotate(0deg);
-    transform: rotate(0deg);
-  }
-  100% {
-    -webkit-transform: rotate(360deg);
-    transform: rotate(360deg);
-  }
-}
-@keyframes load3 {
-  0% {
-    -webkit-transform: rotate(0deg);
-    transform: rotate(0deg);
-  }
-  100% {
-    -webkit-transform: rotate(360deg);
-    transform: rotate(360deg);
-  }
-}
-</style>
+<style scoped></style>
