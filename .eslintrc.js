fdc3b4ade94bee23e4fbccc6baf1a14d28583992
mrLuisFer <mrluisfeer@gmail.com>
2022-02-11 22:12:00 -0600

    add no-unused vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,16 @@ module.exports = {
   },
   extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
   rules: {
-    // override/add rules settings here, such as:
-    // 'vue/no-unused-vars': 'error'
     'vue/require-default-prop': 'off',
     'vue/attribute-hyphenation': 'off',
     'vue/multi-word-component-names': 'off',
     'vue/order-in-components': 'off',
     'vue/attributes-order:': 'off',
+    'vue/no-unused-vars': [
+      'error',
+      {
+        ignorePattern: '^_',
+      },
+    ],
   },
 }
